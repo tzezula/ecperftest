d8e58a165922555511df6b0842aab13f9efcf1f2
Tomas Zezula <tomas.zezula@gmail.com>
2016-01-28 20:53:50 +0100

    JavaScript parsing performance test.

---
+++ JsParsePerfTest/src/main/java/org/netbeans/es/perftest/TestRunner.java
@@ -0,0 +1,209 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2015 Oracle and/or its affiliates. All rights reserved.
+ *
+ * Oracle and Java are registered trademarks of Oracle and/or its affiliates.
+ * Other names may be trademarks of their respective owners.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ *
+ * Contributor(s):
+ *
+ * Portions Copyrighted 2015 Sun Microsystems, Inc.
+ */
+package org.netbeans.es.perftest;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Stream;
+
+final class TestRunner implements Runnable {
+    private final ParserImplementation parser;
+    private final File source;
+    private final Options opts;
+    private final int runs;
+    private final PrintWriter progressWriter;
+    private final PrintWriter reportWriter;
+
+    private TestRunner(
+            final ParserImplementation parser,
+            final File source,
+            final Options opts,
+            final int runs,
+            final PrintWriter progressWriter,
+            final PrintWriter reportWriter) {
+        this.parser = parser;
+        this.source = source;
+        this.opts = opts;
+        this.runs = runs;
+        this.progressWriter = progressWriter;
+        this.reportWriter = reportWriter;
+    }
+
+
+    @Override
+    public void run() {
+        progress(progressWriter, "Parsing %s using %s in %d round(s).%n",
+                source.getName(),
+                parser.getName(),
+                runs);
+        try {
+            final long[] totalTimes = new long[runs];
+            final Map<File,long[]> timesPerFile = new TreeMap<>((f1,f2) -> {
+                    int res = f1.getName().compareTo(f2.getName());
+                    if (res == 0) {
+                        res = f1.getAbsolutePath().compareTo(f2.getAbsolutePath());
+                    }
+                    return res;
+            });
+            for (int i = 0; i < runs; i++) {
+                progress(progressWriter, "Run: %d%n", 1+i);
+                final int fi = i;
+                totalTimes[i] = files(source)
+                        .map((f)->{
+                            final long t = parse(parser, f, opts);
+                            progress(progressWriter, "Parsing %s took: %dms.%n",   //NOI18N
+                                    f.getName(),
+                                    t);
+                            long[] ts = timesPerFile.get(f);
+                            if (ts == null) {
+                                ts = new long[runs];
+                                timesPerFile.put(f, ts);
+                            }
+                            ts[fi] = t;
+                            return t;
+                        })
+                        .reduce(0L, (a,b)->{return a + b;});
+            }
+            timesPerFile.entrySet().stream().forEach((e) -> {
+                report(reportWriter, e.getKey().getName(), e.getValue());
+            });
+            report(reportWriter, "Whole parsing took",totalTimes);    //NOI18N
+        } catch (IOException ioe) {
+            TestRunner.<Void,RuntimeException>sthrow(ioe);
+        }
+
+    }
+
+    private static void progress(
+        final PrintWriter w,
+        final String message,
+        final Object... args) {
+        w.printf(message, args);
+        w.flush();
+    }
+
+    private static void report(
+            final PrintWriter w,
+            final String message,
+            final long[] times) {
+        w.printf("%s:",message);           //NOI18N
+        for (int i=0; i< times.length; i++) {
+            w.printf(" %d : %dms", 1+i, times[i]); //NOI18N
+        }
+        w.println();
+        w.flush();
+    }
+
+    private static long parse (
+        final ParserImplementation parser,
+        final File file,
+        final Options opts) {
+        long st = System.currentTimeMillis();
+        try {
+            parser.parse(file, opts);
+        } catch (IOException ioe) {
+            TestRunner.<Void,RuntimeException>sthrow(ioe);
+        }
+        long time =  System.currentTimeMillis() - st;
+        return time;
+    }
+
+    private static Stream<? extends File> files(final File source) throws IOException {
+        return source.isFile() ?
+                Stream.of(source) :
+                Files.walk(source.toPath())
+                        .filter((p)->p.getFileName().toString().toLowerCase().endsWith(".js"))  //NOI18N
+                        .map((p)->p.toFile());
+    }
+
+    private static <R,T extends Throwable> R sthrow(Throwable t) throws T {
+        throw (T) t;
+    }
+
+    static final class Builder {
+        private final ParserImplementation parser;
+        private final File source;
+        private Options options = new Options(false);
+        private int runs = 1;
+        private PrintWriter progressWriter = new PrintWriter(new OutputStreamWriter(System.out));
+        private PrintWriter reportWriter = progressWriter;
+
+        private Builder(
+                final ParserImplementation parser,
+                final File source) {
+            this.parser = parser;
+            this.source = source;
+        }
+
+        Builder setRunsCount(int runs) {
+            this.runs = runs;
+            return this;
+        }
+
+        Builder setOptions(Options options) {
+            options.getClass();
+            this.options = options;
+            return this;
+        }
+
+        TestRunner build() {
+            return new TestRunner(
+                    parser,
+                    source,
+                    options,
+                    runs,
+                    progressWriter,
+                    reportWriter);
+        }
+
+        static Builder newInstance(
+            final ParserImplementation parser,
+            final File source) {
+            return new Builder(parser, source);
+        }
+    }
+}
---
+++ JsParsePerfTest/src/main/java/org/netbeans/es/perftest/antlr/AntlrParser.java
@@ -0,0 +1,97 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2015 Oracle and/or its affiliates. All rights reserved.
+ *
+ * Oracle and Java are registered trademarks of Oracle and/or its affiliates.
+ * Other names may be trademarks of their respective owners.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ *
+ * Contributor(s):
+ *
+ * Portions Copyrighted 2015 Sun Microsystems, Inc.
+ */
+package org.netbeans.es.perftest.antlr;
+
+import java.io.File;
+import java.io.IOException;
+import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.BailErrorStrategy;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.ConsoleErrorListener;
+import org.antlr.v4.runtime.DefaultErrorStrategy;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.atn.PredictionMode;
+import org.netbeans.es.perftest.Options;
+import org.netbeans.es.perftest.ParserImplementation;
+import org.netbeans.modules.javascript2.editor.parser6.ECMAScript6Lexer;
+import org.netbeans.modules.javascript2.editor.parser6.ECMAScript6Parser;
+
+/**
+ *
+ * @author Tomas Zezula
+ */
+public class AntlrParser implements ParserImplementation {
+    public static final String NAME = "antlr";  //NOI18N
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public void parse(File file, Options options) throws IOException {
+        final ANTLRInputStream in = new ANTLRFileStream(file.getAbsolutePath());
+        final ECMAScript6Lexer lexer = new ECMAScript6Lexer(in);
+        final CommonTokenStream tokens = new CommonTokenStream(lexer);
+        ECMAScript6Parser parser = new ECMAScript6Parser(tokens);
+        parser.removeErrorListeners();
+        parser.getInterpreter().setPredictionMode(PredictionMode.SLL);
+        parser.setErrorHandler(new BailErrorStrategy());
+        ECMAScript6Parser.ProgramContext program;
+        try {
+            program = parser.program();
+        } catch (RuntimeException ex) {
+            if ((ex instanceof RuntimeException) && (ex.getCause() instanceof RecognitionException)) {
+                tokens.reset();
+                parser.setErrorHandler(new DefaultErrorStrategy());
+                if (options.isPrintError()) {
+                    parser.addErrorListener(ConsoleErrorListener.INSTANCE);
+                }
+                parser.getInterpreter().setPredictionMode(PredictionMode.LL);
+                program = parser.program();
+            } else {
+                throw ex;
+            }
+        }
+    }
+}
